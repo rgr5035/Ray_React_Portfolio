f612dadfd30f4704153f16a3c11a152dcaf317e4
Robby Ray <rgr5035@gmail.com>
2021-02-24 13:47:46 -0700

    attempting to understand Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import "@material/react-tab-bar/dist/tab-bar.css";
+import "@material/react-tab-scroller/dist/tab-scroller.css";
+import "@material/react-tab/dist/tab.css";
+import "@material/react-tab-indicator/dist/tab-indicator.css";
+import Tab from "@material/react-tab";
+import TabBar from "@material/react-tab-bar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TabBar>
+        <Tab>
+          <span className="mdc-tab__text-label">One</span>
+        </Tab>
+        ...
+      </TabBar>
+      <button class="mdc-button foo-button">
+        <div class="mdc-button__ripple"></div>
+        <span class="mdc-button__label">Button</span>
+      </button>
     </div>
   );
 }
+
+// mdc.ripple.MDCRipple.attachTo(document.querySelector(".foo-button"));
 
 export default App;
