86dcf933ad7c1de42641f9bb7bca88435580b75b
Robby Ray <rgr5035@gmail.com>
2021-02-24 14:57:37 -0700

    working on component building

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-import "@material/react-tab-bar/dist/tab-bar.css";
-import "@material/react-tab-scroller/dist/tab-scroller.css";
-import "@material/react-tab/dist/tab.css";
-import "@material/react-tab-indicator/dist/tab-indicator.css";
-import Tab from "@material/react-tab";
-import TabBar from "@material/react-tab-bar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Portfolio from "./pages/portfolio";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <TabBar>
-        <Tab>
-          <span className="mdc-tab__text-label">One</span>
-        </Tab>
-        ...
-      </TabBar>
-      <button class="mdc-button foo-button">
-        <div class="mdc-button__ripple"></div>
-        <span class="mdc-button__label">Button</span>
-      </button>
-    </div>
+    <Router>
+      <div>
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
-
-// mdc.ripple.MDCRipple.attachTo(document.querySelector(".foo-button"));
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      Hello World
+      <button class="mdc-button foo-button">
+        <div class="mdc-button__ripple"></div>
+        <span class="mdc-button__label">Button</span>
+      </button>
+    </div>
+  );
+}
+
+export default Home;
